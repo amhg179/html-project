6bc5722a3a1f89a5fb04fed30fb4362d98c3faed
ali <amhg_179@yahoo.com>
2022-01-06 18:55:56 +0200

    your commit message

---
+++ style.js
@@ -0,0 +1,22 @@
+var img1 = document.getElementById("header");
+img1.style.textAlign="right";
+
+var img2 = document.createElement("img");
+img2.src = "dom1.jpg";
+var div = document.getElementsByClassName("center")[0];
+div.append(img2);
+div.style.textAlign="left";
+
+var list = document.getElementById("nav");
+list.style.listStyle="circle inside";
+list.style.textAlign="center";
+
+
+
+
+
+
+
+
+
+
